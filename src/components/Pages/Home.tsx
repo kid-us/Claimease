ae167d0be406e44e356ac9dcde339f5d79d5a6c1
Kidus <98009847+kid-us@users.noreply.github.com>
2024-10-27 04:46:08 -0700

    Add files via upload

---
+++ src/components/Pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facere
+        quos magni veritatis quia explicabo similique ducimus ab maiores! Quos
+        dignissimos ullam officiis natus numquam accusamus voluptate deserunt
+        aperiam iusto?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
